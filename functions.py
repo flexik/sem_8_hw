391286f10c97dd782af653b6a1fa96006baea2e6
Artyome <flex785834@yandex.ru>
2023-05-31 17:11:19 +0300

    добавил доп функции в справочник и файл редми

--- functions.py
+++ functions.py
@@ -14,9 +14,7 @@ def add_data() -> None:
 
 def find_data() -> None:
     """Печатает результат поиска по справочнику."""
-    with open('book.txt', 'r', encoding='utf-8') as file:
-        data = file.read().split('\n')
-        print("\n".join(data))
+    data = open_data()
     data_to_find = input('Введите данные для поиска: ')
     print(search(data, data_to_find))
 
@@ -36,3 +34,33 @@ def search(book: list[str], info: str):
         clarify_data = input("Уточните данные по поиску: ")
         return search(finded_list, clarify_data)
     return 'Совпадений не найдено'
+
+
+def change_data() -> None:
+    """
+    Внесение изменений в справочник (изменение / удаление)
+    """
+    data = open_data()
+    data_to_change = input('Введите данные для изменения/удаления: ')
+    data_to_change = search(data, data_to_change)
+    mode = input('Укажите цифру для взаимодействия с данными: 1 - изменить, 2 - удалить ')
+    if mode == "1":
+        data[data.index(data_to_change)] = add_contact()
+    elif mode == "2":
+        data.remove(data_to_change)
+
+    with open('book.txt', 'w', encoding='utf-8') as file:
+        file.write("\n".join(data))
+
+
+def add_contact() -> str:
+    ''' функция получения контакта для внесения изменений в справочник'''
+    name = input('Введите ФИО для добавления: ')
+    phone = input('Введите телефон для добавления: ')
+    return f"{name} | {phone}"
+
+def open_data() -> None:
+    '''функция считывающая текстовый файл в строку с разделителем переноса строки'''
+    with open('book.txt', 'r', encoding='utf-8') as file:
+        data = file.read().split('\n')
+        return data
